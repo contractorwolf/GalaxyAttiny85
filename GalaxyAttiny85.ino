f97dfe811619797fadcb2ceedc683f24a5f0cdcb
james wolf <contractorwolf@hotmail.com>
2019-11-10 16:13:39 -0500

    switch input and output pins for circuit board changes

--- GalaxyAttiny85.ino
+++ GalaxyAttiny85.ino
@@ -1,24 +1,24 @@
 
-int led = 4;
-int potentiometer = 3;
+int pulseOut = 3;
+int potentiometerIn = A2;
 int pulseMicroSeconds = 42;
 int maxIntervalValue = 540;
 int count = 0;
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin 13 as an output.
-  pinMode(led, OUTPUT);
-  pinMode(potentiometer, INPUT);
+  pinMode(pulseOut, OUTPUT);
+  pinMode(potentiometerIn, INPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  int val = analogRead(potentiometer);
+  int val = analogRead(potentiometerIn);
   val = map(val, 0, 1023, 0, maxIntervalValue);
 
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(pulseOut, HIGH);   // turn the LED on (HIGH is the voltage level)
   delayMicroseconds(pulseMicroSeconds);            // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(pulseOut, LOW);    // turn the LED off by making the voltage LOW
 
   delay(val);
 
